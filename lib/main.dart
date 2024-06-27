89762a528589ca11d4e5e7a3a06b417f36362f8c
Brenda <brewihogora23@gmail.com>
2024-06-27 16:49:14 +0200

    Add the icon nav menu.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'Signup.dart';
-import 'SplashScreen.dart';
+import 'calculator.dart';
+import 'loginScreen.dart';
+import 'mainScreen.dart';
+import 'signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +11,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Navigation Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home:Signup(),
+      home: const MainScreen(),
     );
   }
 }
---
+++ lib/mainScreen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'calculator.dart';
+import 'loginScreen.dart';
+import 'signup.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    MyHomePage(title: 'Calculator'),
+    LoginScreen(),
+    Signup(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: Colors.deepPurple, // Color for the active tab
+        unselectedItemColor: Colors.grey, // Color for the inactive tabs
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Calculator',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Login',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_add),
+            label: 'Signup',
+          ),
+        ],
+      ),
+    );
+  }
+}
